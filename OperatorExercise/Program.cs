001da6bb104b6605d224b5e448c538295237da66
Michael Bohannon <michaelbohannon123@gmail.com>
2022-12-11 15:57:02 -0500

    Finsihed Operator Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -3,8 +3,38 @@
     public class Program
     {
         static void Main(string[] args)
+
+            // int is explicit typing
+            // var is inferred typing
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            if (a == 17 && b == 4)
+
+                // Concatination
+                // Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+
+                // String Interpolation
+                Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+            
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine($"The area of a circle with a radius of {userInput} is {AreaOfCircle(radius)}");
+
+
         }
+
+            public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+
     }
 }
